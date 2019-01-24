cae1987edb7f3b64402ddf1025a5a4ddff405df6
Jakapo <jakapo.simek95@gmail.com>
2019-01-24 14:26:35 +0100

    Event Emitting

--- projekt1/skript.js
+++ projekt1/skript.js
@@ -1,9 +1,30 @@
+var events = require('events');
+var util = require('util')
+
+var Person = function(name) {
+  this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+
+var jakub = new Person('Jakub');
+var filip = new Person('Filip');
+var vika = new Person('Vika');
+var people = [jakub,filip,vika];
+
+people.forEach(function(Person) {
+  Person.on('speak', function(msg) {
+    console.log(Person.name + ' speak:' + msg);
+  });
+});
+
+jakub.emit('speak', 'ahoj svet');
+/*
 var stuff = require('./libs');
 
 console.log(stuff.counter(['shaun', 'crystal', 'ryu']));
 console.log(stuff.adder(stuff.pi,6));
 
-/*
 function callFunction(fun) {
 fun();
 }
