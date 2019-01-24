6dd8af4e53f09887786edd30d8aa9523206cb658
Jakapo <jakapo.simek95@gmail.com>
2019-01-24 14:05:41 +0100

    Module patterns nodejs

--- projekt1/skript.js
+++ projekt1/skript.js
@@ -1,1 +1,33 @@
-console.log("Hello World!");
+var stuff = require('./libs');
+
+console.log(stuff.counter(['shaun', 'crystal', 'ryu']));
+console.log(stuff.adder(stuff.pi,6));
+
+/*
+function callFunction(fun) {
+fun();
+}
+
+//function expression
+var sayBye = function () {
+console.log("bye!");
+}
+
+callFunction(sayBye);
+
+
+console.log(__dirname);
+console.log(__filename);
+
+var time = 0;
+
+var timer = setInterval(timeTrack, 2000);
+
+function timeTrack() {
+  time += 2;
+  console.log(time + " seconds passed");
+  if (time > 5) {
+    clearInterval(timer);
+  }
+}
+*/
